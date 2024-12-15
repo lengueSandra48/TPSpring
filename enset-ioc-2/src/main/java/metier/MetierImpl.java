061b757260ea9f61d46754e80945e5d9b43b2efe
Sandra lengue <lenguesandra@gmail.com>
2024-12-15 16:30:05 +0100

    Change the project structure of my project

--- enset-ioc-2/src/main/java/metier/MetierImpl.java
+++ enset-ioc-2/src/main/java/metier/MetierImpl.java
@@ -1,6 +1,7 @@
 package metier;
 
 import dao.IDao;
+import extension.IDaoImp2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,6 @@ import org.springframework.stereotype.Component;
 public class MetierImpl implements IMetier{
     //au moment d'instancier metierImpl, si tu trouve l'objet de type IDao parmis les objets deja crees tu l'injectes dans la variable "dao"
     @Autowired //injection des dependances
-    //@Qualifier("dao2") //ici lorsqu'une interface a plusieurs implementations, on specifie celle qui est injecte.
     private IDao dao; //couplage faible
 
     public MetierImpl(IDao dao) {
